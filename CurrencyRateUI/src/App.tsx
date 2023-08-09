6ce2d3922c6ce4cc0d65bff74de27cf350aa8518
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-08-09 10:54:59 +0300

    Rewrite App container

--- CurrencyRateUI/src/App.tsx
+++ CurrencyRateUI/src/App.tsx
@@ -1,5 +1,6 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import { useContext } from "react";
-import "./App.css";
 import CurrencyInfo from "./components/CurrencyInfo/CurrencyInfo";
 import Details from "./components/Details/Details";
 import { DetailsContext } from "./context/details";
@@ -8,10 +9,21 @@ function App() {
   const { detailsVisibility } = useContext(DetailsContext);
 
   return (
-    <div className="app">
-      <CurrencyInfo className="app_currency-info" />
-      {detailsVisibility && <Details className="app__details" />}
-    </div>
+    <Container sx={{ display: "grid", placeItems: "center", height: "100vh" }}>
+      <Box
+        display="flex"
+        width={750}
+        boxSizing="border-box"
+        padding={3}
+        border="1px solid #d9d9d9"
+        borderRadius="15px"
+        gap={3}
+        height="fit-content"
+      >
+        <CurrencyInfo className="app_currency-info" />
+        {detailsVisibility && <Details className="app__details" />}
+      </Box>
+    </Container>
   );
 }
 
